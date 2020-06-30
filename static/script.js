2f30cd88b95a6a7684a424224a9ad301e6a16bb6
Mac User <macuser@Macs-Macintosh.local>
2020-06-30 13:33:36 -0700

    First commit

---
+++ static/script.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    document.querySelector('#submit').onclick = () => {
+        console.log('test');
+    };
+    // // Connect to websocket
+    // var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+
+    // // When connected, configure buttons
+    // socket.on('connect', () => {
+
+    //     // Each button should emit a "submit vote" event
+    //     // document.querySelectorAll('button').forEach(button => {
+    //     //     button.onclick = () => {
+    //     //         const selection = button.dataset.vote;
+    //     //         socket.emit('submit vote', {'selection': selection});
+    //     //     };
+    //     // });
+        
+    // });
+
+    // // When a new vote is announced, add to the unordered list
+    // socket.on('announce vote', data => {
+    //     const li = document.createElement('li');
+    //     li.innerHTML = `Vote recorded: ${data.selection}`;
+    //     document.querySelector('#votes').append(li);
+    // });
+
+    // socket.on('vote totals', data => {
+    //     document.querySelector('#yes').innerHTML = data.yes;
+    //     document.querySelector('#no').innerHTML = data.no;
+    //     document.querySelector('#maybe').innerHTML = data.maybe;
+    // });
+});
+
+let hello = async () => {return "Hello"};
+hello().then(console.log);
+
+
+alert("check");
